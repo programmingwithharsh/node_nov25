b4ae1fe88c6090730dfcaa04513879b51e2d937f
Harsh Tiwari <harshtiwari343@email.com>
2025-11-26 10:14:12 +0530

    Update CRUD API

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-import express from 'express'; // es6 import module
-
+// import express from 'express'; // es6 import module
+const express = require('express'); // es5 import module
+const cors = require('cors'); // es5 import module
 const app = express();
+app.use(cors());
 app.use(express.json());
+
 // GET ALL API
 app.get('/', (req, res) => {
     let users = [
@@ -12,25 +15,43 @@ app.get('/', (req, res) => {
             password: 'Aditi123'
         },
         {
-            id: 1,
+            id: 2,
             fullname: 'Amit',
             email: 'Amit@gmail.com',
             password: 'Amit123'
+        },
+        {
+            id: 3,
+            fullname: 'Devender',
+            email: 'Devender@gmail.com',
+            password: 'Devender'
+        },
+        {
+            id: 4,
+            fullname: 'Dinesh',
+            email: 'Dinesh@gmail.com',
+            password: 'Dinesh'
+        },
+        {
+            id: 5,
+            fullname: 'Gaurav',
+            email: 'Gaurav@gmail.com',
+            password: 'Gaurav'
         }
     ]
 
     // res.send('This is node.js GET API')
     res.json(users);
 })
-
+// POST API
 app.post('/', (req, res) => {
     res.json({ 'status': 'post request', 'body': req.body.name })
 })
-
+// PUT API
 app.put('/', (req, res) => {
     res.json({ 'status': 'put request', 'body': req.body.name })
 })
-
+// DELETE API
 app.delete('/', (req, res) => {
     res.json({ 'status': 'delete request' })
 })
